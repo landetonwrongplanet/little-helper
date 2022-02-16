f9b4508aefe1b5679c08a72fe6a0c39e41f89457
landetonwrongplanet <mail@ileniacastano.ch>
2022-02-16 13:54:35 +0100

    changed store

--- store/socialcompetences.js
+++ store/socialcompetences.js
@@ -1,42 +1,42 @@
 export const state = () => ({
-  mobiledoc: [{
-    kardextitle: 'Hausaufgaben vergessen',
-    kardextext: 'Heute habe ich meine Hausaufgaben vergessen! Welch eine Schande!',
+  socials: [{
+    topictitle: 'Hausaufgaben vergessen',
+    topictext: 'Heute habe ich meine Hausaufgaben vergessen! Welch eine Schande!',
     done: false,
     date: '1629270742839'
   }, {
-    kardextitle: 'Goldfisch rutschte auf dem Parkett aus',
-    kardextext: 'Heute morgen rutschte mein Goldfisch im Wohnzimmer auf seiner Flosse aus',
+    topictitle: 'Goldfisch rutschte auf dem Parkett aus',
+    topictext: 'Heute morgen rutschte mein Goldfisch im Wohnzimmer auf seiner Flosse aus',
     done: true,
     date: '1629270742839'
   }, {
-    kardextitle: 'Essen verbrannt',
-    kardextext: 'Meine Pfanne wurde zu heiss, das Schnitzel verkohlt',
+    topictitle: 'Essen verbrannt',
+    topictext: 'Meine Pfanne wurde zu heiss, das Schnitzel verkohlt',
     done: false,
     date: '1629270742839'
   }, {
-    kardextitle: 'Cellosaite gerissen',
-    kardextext: 'Beim Celloüben riss mir eine Saite. ',
+    topictitle: 'Cellosaite gerissen',
+    topictext: 'Beim Celloüben riss mir eine Saite. ',
     done: false,
     date: '1629270742839'
   }]
 })
 export const mutations = {
-  addMedicalMemo (state, addedMedicalMemo) {
-    state.mobiledoc.push({
-      kardextitle: addedMedicalMemo.kardextitle,
-      kardextext: addedMedicalMemo.kardextext,
+  addScocialTopic (state, addedSocialTopic) {
+    state.socials.push({
+      topictitle: addedSocialTopic.topictitle,
+      topictext: addedSocialTopic.topictext,
       done: false,
-      date: addedMedicalMemo.date
+      date: addedSocialTopic.date
     })
   },
-  deleteMedicalMemo (state, deletedMedicalMemo) {
-    const medicalIndex = state.mobiledoc.indexOf(deletedMedicalMemo)
-    state.mobiledoc.splice(medicalIndex, 1)
+  deleteSocialTopic (state, deletedSocialTopic) {
+    const socialIndex = state.socially.indexOf(deletedSocialTopic)
+    state.socially.splice(socialIndex, 1)
   },
-  updateMedicalMemo (state, updatedMedicalMemo) {
-    const medicalIndex = state.mobiledoc.indexOf(updatedMedicalMemo.old)
-    state.mobiledoc[medicalIndex].kardextitle = updatedMedicalMemo.new.kardextitle
-    state.mobiledoc[medicalIndex].kardextext = updatedMedicalMemo.new.kardextext
+  updateSocialTopic (state, updatedSocialTopic) {
+    const socialIndex = state.socially.indexOf(updatedSocialTopic.old)
+    state.socially[socialIndex].topictitle = updatedSocialTopic.new.topictitle
+    state.socially[socialIndex].topictext = updatedSocialTopic.new.topictext
   }
 }
